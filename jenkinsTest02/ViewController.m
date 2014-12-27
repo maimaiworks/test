ca2af46458ce6f2236afae4f48358a1300651dfa
Hiroshi Imai <im@i-hiroshi.com>
2014-12-27 17:48:53 +0900

    cocoaPods, jenkinsScript

--- jenkinsTest02/ViewController.m
+++ jenkinsTest02/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "UIImageView+WebCache.h"
 
 @interface ViewController ()
 
@@ -27,6 +28,17 @@
 	label.backgroundColor = [UIColor lightGrayColor];
 	[self.view addSubview:label];
 
+}
+
+
+-(void)viewWillAppear:(BOOL)animated{
+	[super viewWillAppear:animated];
+	
+	NSURL* url = [NSURL URLWithString:@"https://dl.dropboxusercontent.com/u/36343998/user.png"];
+	UIImageView* iv = [[UIImageView alloc] initWithFrame:CGRectMake(20, 20, 64, 64)];
+	[iv sd_setImageWithURL:url placeholderImage:nil options:SDWebImageCacheMemoryOnly];
+	[self.view addSubview:iv];
+	
 }
 
 - (void)didReceiveMemoryWarning {
